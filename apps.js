5db146b774fde840e55bb6e2006b81fe74ecd95f
Keegan18 <kolbeinsen.tafaoa@gmail.com>
2017-11-20 11:30:39 -1000

    Update 2

--- apps.js
+++ apps.js
@@ -1,9 +1,22 @@
-
-var x = 24;
-var y = 65;
-var z = 198;
-
+//Number variables//
+var output;
 
 function calculator(x, y, z){
-
+if (x == "add", "sum",) {
+	output = y + z;
+		}
+		if else (x == "subtract", "difference",){
+			output = y - z;
+		}
+		if else (x == "multiply", "product"){
+			output = y * z;
+		}
+		if else (x == "divide", "quotient"){
+			output = y / z;
+		}
+else {
+	output = "Error, remove McAfee and install Norton"
 }
+}
+calculator ("multiply", 34, 59);
+console.log (output);
